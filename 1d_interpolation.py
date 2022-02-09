3d3b085ba4f596244ed03e274cb95dcd6eb44959
Andrei <inwaves@live.com>
2022-02-09 08:34:35 +0000

    Port to pytorch-lightning

--- 1d_interpolation.py
+++ 1d_interpolation.py
@@ -1,5 +1,5 @@
 import numpy as np
-import torch.nn as nn
+import pytorch_lightning as pl
 import torch
 
 from torch.utils.data import DataLoader
@@ -12,48 +12,10 @@ from utils.plotting import plot_data
 device = "cpu"
 n = 10
 
-# TODO: port me to Pytorch Lightning!
 # TODO: start with a "large" learning rate, halve it until loss starts to decrease.
 # TODO: implement stopping condition for training: loss(t)- loss(t-1) < 10^{-8}.
 # TODO: implement anti-symmetric initialisation.
 # TODO: solve variational problem over the domain, calculate infinity norm of difference.
-
-
-def train(dataloader, model, loss_fn, optimiser):
-    """Train model for one epoch."""
-    size = len(dataloader.dataset)
-    model.train()
-    for i, batch in enumerate(dataloader):
-        x, y = batch[:, 0].float().unsqueeze(1), batch[:, 1].float().unsqueeze(1)
-
-        # Compute prediction error.
-        pred = model(x)
-
-        # print(f"pred: {pred}")
-        loss = loss_fn(pred, y)
-
-        # Back-propagate error.
-        optimiser.zero_grad()
-        loss.backward()
-        optimiser.step()
-
-        return loss.item()
-
-
-def test(dataloader, model, loss_fn):
-    size = len(dataloader.dataset)
-    num_batches = len(dataloader)
-    model.eval()  # Sets the model to evaluation mode, no gradients computed.
-    test_loss, correct = 0, 0
-    with torch.no_grad():
-        for i, batch in enumerate(dataloader):
-            x, y = batch[:, 0].float().unsqueeze(1), batch[:, 1].float().unsqueeze(1)
-            pred = model(x)
-            test_loss += loss_fn(pred, y).item()
-            correct += (pred == y).sum().item()
-    test_loss /= num_batches
-    correct /= size
-    print(f"Test error: \n Accuracy: {(100 * correct):>0.1f}%, avg. loss: {test_loss:>8f}\n")
 
 
 def generate_data():
@@ -66,25 +28,23 @@ def generate_data():
     training_data = np.array(list(zip(x, y)))
     test_data = np.array(list(zip(x_test, y_test)))
 
-    # Split data into batches for training.
-    batch_size = len(x)
-    train_dataloader = DataLoader(training_data, batch_size=batch_size)
-    test_dataloader = DataLoader(test_data, batch_size=batch_size)
+    # We're doing full-batch gradient descent, so the batch_size = n
+    train_dataloader = DataLoader(training_data, batch_size=len(x))
+    test_dataloader = DataLoader(test_data, batch_size=len(x_test))
     return train_dataloader, test_dataloader, x_test, y_test
 
 
 if __name__ == '__main__':
+    pl.seed_everything(1337)
+
     train_dataloader, test_dataloader, x_test, y_test = generate_data()
     model = ShallowRelu(n, 1, 1).to(device).float()
-    loss_fn = nn.MSELoss()
-    optimiser = torch.optim.SGD(model.parameters(), lr=1e-5)
 
-    epochs = 500
-    for t in range(epochs):
-        loss = train(train_dataloader, model, loss_fn, optimiser)
-        print(f"Epoch {t + 1:>5d} loss: {loss:>8f}")
+    epochs = 100
+    trainer = pl.Trainer(max_epochs=epochs, callbacks=[])
+    trainer.fit(model, train_dataloader)
 
-    test(test_dataloader, model, loss_fn)
+    trainer.test(test_dataloaders=test_dataloader)
 
     y_pred = model(torch.tensor(x_test).float().unsqueeze(1).to(device))
     plot_data(x_test, y_pred.detach().numpy(), y_test)
--- models/shallow_relu.py
+++ models/shallow_relu.py
@@ -1,12 +1,40 @@
+import torch
 import torch.nn as nn
+import torch.nn.functional as F
+import pytorch_lightning as pl
 
 
-class ShallowRelu(nn.Module):
-    def __init__(self, n, input_dim, output_dim):
-        super(ShallowRelu, self).__init__()
+class ShallowRelu(pl.LightningModule):
+    def __init__(self,
+                 n,
+                 input_dim,
+                 output_dim,) -> None:
+        super().__init__()
+
+        self.save_hyperparameters()
         self.hidden = nn.Linear(input_dim, n)
         self.relu = nn.ReLU()
         self.out = nn.Linear(n, output_dim)
 
     def forward(self, x):
         return self.out(self.relu(self.hidden(x)))
+
+    def training_step(self, batch, batch_idx):
+        idx, targets = batch[:, 0].float().unsqueeze(1), batch[:, 1].float().unsqueeze(1)
+        out = self.forward(idx)
+
+        loss = F.mse_loss(out, targets)
+        self.log("train_loss", loss)
+        return loss
+
+    def test_step(self, batch, batch_idx):
+        idx, targets = batch[:, 0].float().unsqueeze(1), batch[:, 1].float().unsqueeze(1)
+        out = self.forward(idx)
+
+        loss = F.mse_loss(out, targets)
+        self.log("test_loss", loss)
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.SGD(self.parameters(), lr=1e-5)
+        return optimizer
