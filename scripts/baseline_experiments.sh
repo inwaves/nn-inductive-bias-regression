a8abd231d99f0d082c53d0f6d1d69f7f134d32e6
Andrei <inwaves@live.com>
2022-04-06 14:21:22 +0100

    Add job script for CSD3

--- scripts/baseline_experiments.sh
+++ scripts/baseline_experiments.sh
@@ -1,1 +1,8 @@
+#/bin/bash
+#SBATCH --account KRUEGER-SL3-GPU
+#SBATCH --pascal pascal
+#SBATCH --nodes 1
+#SBATCH --gres=gpu:1
+#SBATCH --exclusive
+
 python3 1d_regression.py --dataset=sine --generalisation_task=baseline --model=ASIShallowRelu --hidden_units=10 --learning_rate=0.001
