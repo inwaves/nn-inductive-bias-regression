c6dde744c19e77111e7f0641f525b5d86440bf95
Andrei <inwaves@live.com>
2022-04-11 17:50:11 +0100

    One more

--- models/shallow_relu.py
+++ models/shallow_relu.py
@@ -77,8 +77,11 @@ class AsiShallowRelu(pl.LightningModule):
         print(f"In the ASIShallowRelu initialiser, the hidden weights are on these devices: {self.hidden1.weight.device}, {self.hidden2.weight.device}")
 
     def forward(self, x):
-        path1 = self.out1(self.relu(self.hidden1(x)).to(device))
-        path2 = self.out2(self.relu(self.hidden2(x)).to(device))
+        print(f"In the ASIShallowRelu *function*, the weights are on these devices: {self.out1.weight.device}, {self.out2.weight.device}")
+        print(f"In the ASIShallowRelu forward function, the hidden weights are on these devices: {self.hidden1.weight.device}, {self.hidden2.weight.device}")
+
+        path1 = self.out1(self.relu(self.hidden1(x)))
+        path2 = self.out2(self.relu(self.hidden2(x)))
         return (torch.sqrt(torch.tensor([2]).to(device)) / 2) * path1 + (torch.sqrt(torch.tensor([2]).to(device)) / 2) * path2
 
     def training_step(self, batch, batch_idx):
