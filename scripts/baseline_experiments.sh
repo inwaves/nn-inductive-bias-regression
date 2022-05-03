22c363280aaae62b9fa3a6b127442690ac15c291
Andrei <inwaves@live.com>
2022-05-03 13:20:17 +0300

    all baselines

--- scripts/baseline_experiments.sh
+++ scripts/baseline_experiments.sh
@@ -32,4 +32,4 @@
 
 for i in {1..5}; do python3 1d_regression.py --dataset=sine --generalisation_task=baseline --model=ASIShallowRelu --hidden_units=10 --learning_rate=0.1 --adjust_data_linearly=True; done
 for i in {1..5}; do python3 1d_regression.py --dataset=sine --generalisation_task=baseline --model=ASIShallowRelu --hidden_units=100 --learning_rate=0.01 --adjust_data_linearly=True; done
-# for i in {1..5}; do python3 1d_regression.py --dataset=sine --generalisation_task=baseline --model=ASIShallowRelu --hidden_units=1000 --learning_rate=0.0001 --adjust_data_linearly=True; done
+for i in {1..5}; do python3 1d_regression.py --dataset=sine --generalisation_task=baseline --model=ASIShallowRelu --hidden_units=1000 --learning_rate=0.001 --adjust_data_linearly=True; done
