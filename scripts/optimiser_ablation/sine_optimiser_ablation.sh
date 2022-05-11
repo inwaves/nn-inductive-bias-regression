72bc36f3e70189711c6f8d1fb41cea96a4955807
Andrei <inwaves@live.com>
2022-05-11 11:07:34 +0300

    Prepare scripts for ablations

---
+++ scripts/optimiser_ablation/sine_optimiser_ablation.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#!
+#! Example SLURM job script for Wilkes3 (AMD EPYC 7763, ConnectX-6, A100)
+#!
+#! sbatch directives begin here ###############################
+#! Name of the job:
+#SBATCH -J sine-optimiser-ablation
+#! Which project should be charged (NB Wilkes2 projects end in '-GPU'):
+#SBATCH -A KRUEGER-SL2-GPU
+#! How many whole nodes should be allocated?
+#SBATCH --nodes=1
+#! How many (MPI) tasks will there be in total?
+#! Note probably this should not exceed the total number of GPUs in use.
+#SBATCH --ntasks=1
+#! Specify the number of GPUs per node (between 1 and 4; must be 4 if nodes>1).
+#! Note that the job submission script will enforce no more than 3 cpus per GPU.
+#SBATCH --gres=gpu:1
+#! How much wallclock time will be required?
+#SBATCH --time=05:00:00
+#! What types of email messages do you wish to receive?
+#SBATCH --mail-type=FAIL
+#! Uncomment this to prevent the job from being requeued (e.g. if
+#! interrupted by node failure or system downtime):
+##SBATCH --no-requeue
+
+#SBATCH --output=slurm-out/%x.%j.out
+
+
+#! Do not change:
+#SBATCH -p ampere
+#/bin/bash
+set -x #echo on
+start=$(date +%s)
+
+1d_regression.py --dataset=sine --optimiser=sgd --nonlinearity=relu --generalisation_task=baseline --model=ASIShallowRelu --hidden_units=10 --learning_rate=0.1 --adjust_data_linearly=True
+1d_regression.py --dataset=sine --optimiser=sgd --nonlinearity=relu --generalisation_task=baseline --model=ASIShallowRelu --hidden_units=100 --learning_rate=0.01 --adjust_data_linearly=True
+1d_regression.py --dataset=sine --optimiser=sgd --nonlinearity=relu --generalisation_task=baseline --model=ASIShallowRelu --hidden_units=1000 --learning_rate=0.001 --adjust_data_linearly=True
+
+1d_regression.py --dataset=sine --optimiser=adam --nonlinearity=relu --generalisation_task=baseline --model=ASIShallowRelu --hidden_units=10 --learning_rate=0.1 --adjust_data_linearly=True
+1d_regression.py --dataset=sine --optimiser=adam --nonlinearity=relu --generalisation_task=baseline --model=ASIShallowRelu --hidden_units=100 --learning_rate=0.01 --adjust_data_linearly=True
+1d_regression.py --dataset=sine --optimiser=adam --nonlinearity=relu --generalisation_task=baseline --model=ASIShallowRelu --hidden_units=1000 --learning_rate=0.001 --adjust_data_linearly=True
+
+1d_regression.py --dataset=sine --optimiser=momentum --nonlinearity=relu --generalisation_task=baseline --model=ASIShallowRelu --hidden_units=10 --learning_rate=0.1 --adjust_data_linearly=True
+1d_regression.py --dataset=sine --optimiser=momentum --nonlinearity=relu --generalisation_task=baseline --model=ASIShallowRelu --hidden_units=100 --learning_rate=0.01 --adjust_data_linearly=True
+1d_regression.py --dataset=sine --optimiser=momentum --nonlinearity=relu --generalisation_task=baseline --model=ASIShallowRelu --hidden_units=1000 --learning_rate=0.001 --adjust_data_linearly=True
+
+end=$(date +%s)
+
+runtime=$((end-start))
