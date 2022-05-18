b24b9251ed9eaac987c1a57f535ee8b785204486
Andrei <inwaves@live.com>
2022-05-18 19:25:43 +0100

    Fix parabola ds

--- scripts/early_stopping_ablation/sq-1k.sh
+++ scripts/early_stopping_ablation/sq-1k.sh
@@ -32,7 +32,7 @@ num_iter=1
 
 for ((i=1;i<=num_iter;i++))
 do
-  python3 1d_regression.py --tag=sq-no-es-1k --early_stopping=False --num_epochs=100000 --lr_schedule=plateau --dataset=square --generalisation_task=baseline --model=ASIShallowRelu --hidden_units=1000 --learning_rate=0.001 --adjust_data_linearly=True
+  python3 1d_regression.py --tag=sq-no-es-1k --num_datapoints=50 --early_stopping=False --num_epochs=100000 --lr_schedule=plateau --dataset=square --generalisation_task=baseline --model=ASIShallowRelu --hidden_units=1000 --learning_rate=0.001 --adjust_data_linearly=True
 done
 
 end=$(date +%s)
