65b886b7cf89aac576d594b12fb00fc14f41bbf5
Andrei <inwaves@live.com>
2022-05-20 16:35:44 +0100

    Bug fixes and error bars

--- average_error.py
+++ average_error.py
@@ -7,7 +7,7 @@ num_runs = 3
 categories = ["square"]
 
 
-def loglogplot(x, average_errors, category):
+def loglogplot(x, average_errors, standard_deviations, category):
     """This follows the method in the authors' code."""
     X = np.log(x).reshape(-1, 1)
     ys = np.log(average_errors)
@@ -18,6 +18,7 @@ def loglogplot(x, average_errors, category):
     plt.figure()
     plt.title(f"Error for {category} as network scales")
     plt.loglog(x, average_errors, "-o")
+    plt.errorbar(x, average_errors, yerr=standard_deviations, fmt="none", capsize=5)
     plt.loglog(Xline, np.exp(reg.predict(np.log(Xline))))
     plt.loglog(Xline, 1/np.sqrt(Xline))
     plt.legend([f"Error",
@@ -30,17 +31,18 @@ def loglogplot(x, average_errors, category):
 
 
 if __name__ == '__main__':
-    x = np.array([10000, 50000, 100000])
+    x = np.array([10, 40, 160, 640, 10240])
 
     with open("logs/baseline_errors.txt", "r") as f:
         lines = f.readlines()
 
     lines = np.array([line.replace("\n", "").split(",")[1:] for line in lines], dtype=np.float32)
     for category in categories:
-        average_errors = []
+        avg_errs, st_devs = [], []
         for i, _ in enumerate(x):
-            average_errors.append(np.mean(lines[i*num_runs: (i+1)*num_runs, 1]))
-        print(f"{category} average errors: {average_errors}")
-        loglogplot(x, average_errors, category)
+            avg_errs.append(np.mean(lines[i * num_runs: (i + 1) * num_runs, 1]))
+            st_devs.append(np.std(lines[i * num_runs: (i + 1) * num_runs, 1]))
+        print(f"{category} average errors: {avg_errs}")
+        loglogplot(x, avg_errs, st_devs, category)
         lines = lines[len(x)*num_runs:]
 
