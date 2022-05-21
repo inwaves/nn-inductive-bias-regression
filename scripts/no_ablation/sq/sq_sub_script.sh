c3a44905924618864b8c8796bef4403f03daea81
Andrei <inwaves@live.com>
2022-05-21 22:09:04 +0100

    Try square again

--- scripts/no_ablation/sq/sq_sub_script.sh
+++ scripts/no_ablation/sq/sq_sub_script.sh
@@ -53,7 +53,7 @@ module load rhel8/default-amp              # REQUIRED - loads the basic environm
 #! Insert additional module load commands after this line if needed:
 
 #! Full path to application executable:
-application="./scripts/no_ablation/square_var_10240.sh"
+application="./scripts/no_ablation/square_var_10.sh"
 options=""
 
 #! Work directory (i.e. where the job will run):
