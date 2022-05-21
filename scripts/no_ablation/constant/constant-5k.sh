550447d6493123d5785ca544c165fe99d8631933
Andrei <inwaves@live.com>
2022-05-21 19:24:44 +0100

    Constant

---
+++ scripts/no_ablation/constant/constant-5k.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#!
+#! Example SLURM job script for Wilkes3 (AMD EPYC 7763, ConnectX-6, A100)
+#!
+#! sbatch directives begin here ###############################
+#! Name of the job:
+#SBATCH -J const-es-5000
+#! Which project should be charged (NB Wilkes2 projects end in '-GPU'):
+#SBATCH -A KRUEGER-SL2-CPU
+#! How many whole nodes should be allocated?
+#SBATCH --nodes=1
+#! How many (MPI) tasks will there be in total?
+#! Note probably this should not exceed the total number of GPUs in use.
+#SBATCH --ntasks=1
+#! How much wallclock time will be required?
+#SBATCH --time=36:00:00
+#! What types of email messages do you wish to receive?
+#SBATCH --mail-type=FAIL
+#! Uncomment this to prevent the job from being requeued (e.g. if
+#! interrupted by node failure or system downtime):
+##SBATCH --no-requeue
+
+#SBATCH --output=slurm-out/%x.%j.out
+
+
+#! Do not change:
+#SBATCH -p icelake
+#/bin/bash
+set -x #echo on
+start=$(date +%s)
+num_iter=1
+
+for ((i=1;i<=num_iter;i++))
+do
+  python3 1d_regression.py --tag=const-es-5000 --early_stopping=True --num_epochs=100000 --lr_schedule=plateau --dataset=constant --generalisation_task=baseline --model_type=ASIShallowRelu --hidden_units=5000 --learning_rate=0.0005 --adjust_data_linearly=False
+done
+
+end=$(date +%s)
+
+runtime=$((end-start))
+
+echo $runtime
