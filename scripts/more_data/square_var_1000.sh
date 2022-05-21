bd387c7138e4bc269f09028c2cac51c0571820c6
Andrei <inwaves@live.com>
2022-05-21 22:17:59 +0100

    More data

--- scripts/more_data/square_var_1000.sh
+++ scripts/more_data/square_var_1000.sh
@@ -32,7 +32,7 @@ num_iter=1
 
 for ((i=1;i<=num_iter;i++))
 do
-  python3 1d_regression.py --tag=relu-sq2560 --dataset=square --generalisation_task=baseline --model_type=ShallowRelu --hidden_units=2560 --learning_rate=0.001 --adjust_data_linearly=True --early_stopping=True --num_epochs=100000  --num_datapoints=50
+  python3 1d_regression.py --tag=relu-sq2560 --dataset=square --generalisation_task=baseline --model_type=ShallowRelu --hidden_units=2560 --learning_rate=0.0005 --adjust_data_linearly=True --early_stopping=True --num_epochs=100000  --num_datapoints=50
 done
 
 end=$(date +%s)
