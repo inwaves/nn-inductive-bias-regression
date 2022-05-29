42463cf08b9acbb944de8a431d4deebeab095cfa
Andrei <inwaves@live.com>
2022-05-29 10:52:25 +0100

    Constant and linear with adjusted

--- scripts/no_ablation/linear/linear-10.sh
+++ scripts/no_ablation/linear/linear-10.sh
@@ -4,7 +4,7 @@ num_iter=1
 
 for ((i=1;i<=num_iter;i++))
 do
-  python3 1d_regression.py --tag=lin-es-10 --early_stopping=True --num_epochs=100000 --lr_schedule=plateau --dataset=linear --generalisation_task=baseline --model_type=ASIShallowRelu --hidden_units=10 --learning_rate=0.01 --adjust_data_linearly=False
+  python3 1d_regression.py --tag=lin-es-10 --early_stopping=True --num_epochs=100000 --lr_schedule=plateau --dataset=linear --generalisation_task=baseline --model_type=ASIShallowRelu --hidden_units=10 --learning_rate=0.01 --adjust_data_linearly=True
 done
 
 end=$(date +%s)
--- scripts/no_ablation/linear/linear-10k.sh
+++ scripts/no_ablation/linear/linear-10k.sh
@@ -4,7 +4,7 @@ num_iter=1
 
 for ((i=1;i<=num_iter;i++))
 do
-  python3 1d_regression.py --tag=lin-es-10k --early_stopping=True --num_epochs=100000 --lr_schedule=plateau --dataset=linear --generalisation_task=baseline --model_type=ASIShallowRelu --hidden_units=10000 --learning_rate=0.0001 --adjust_data_linearly=False
+  python3 1d_regression.py --tag=lin-es-10k --early_stopping=True --num_epochs=100000 --lr_schedule=plateau --dataset=linear --generalisation_task=baseline --model_type=ASIShallowRelu --hidden_units=10000 --learning_rate=0.0001 --adjust_data_linearly=True
 done
 
 end=$(date +%s)
