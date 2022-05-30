867d5ad56124b9322a1534ec23278d972c93df58
Andrei <inwaves@live.com>
2022-05-30 17:25:50 +0100

    MLP unadjusted

--- scripts/model_ablation/mlp-1k.sh
+++ scripts/model_ablation/mlp-1k.sh
@@ -4,7 +4,7 @@ num_iter=1
 
 for ((i=1;i<=num_iter;i++))
 do
-  python3 1d_regression.py --tag=mlp-"$1"-150 --early_stopping=True --lr_schedule=plateau --dataset="$1" --generalisation_task=baseline --model_type=MLP --hidden_units=1000 --learning_rate=0.00001 --adjust_data_linearly=True
+  python3 1d_regression.py --tag=mlp-"$1"-150 --early_stopping=True --lr_schedule=plateau --dataset="$1" --generalisation_task=baseline --model_type=MLP --hidden_units=1000 --learning_rate=0.00001 --adjust_data_linearly=False
 done
 
 end=$(date +%s)
