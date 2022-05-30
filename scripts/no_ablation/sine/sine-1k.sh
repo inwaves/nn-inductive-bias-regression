048ecd076141d654aba5fc7fc2e1145382166269
Andrei <inwaves@live.com>
2022-05-30 14:27:35 +0100

    Back to ASI to rerun constant and linear

--- scripts/no_ablation/sine/sine-1k.sh
+++ scripts/no_ablation/sine/sine-1k.sh
@@ -4,7 +4,7 @@ num_iter=1
 
 for ((i=1;i<=num_iter;i++))
 do
-  python3 1d_regression.py --tag=sine-no-es-1k --early_stopping=True --num_epochs=100000 --lr_schedule=plateau --dataset=sine --generalisation_task=baseline --model_type=ShallowRelu --hidden_units=1000 --learning_rate=0.001 --adjust_data_linearly=True
+  python3 1d_regression.py --tag=sine-no-es-1k --early_stopping=True --num_epochs=100000 --lr_schedule=plateau --dataset=sine --generalisation_task=baseline --model_type=ASIShallowRelu --hidden_units=1000 --learning_rate=0.001 --adjust_data_linearly=True
 done
 
 end=$(date +%s)
