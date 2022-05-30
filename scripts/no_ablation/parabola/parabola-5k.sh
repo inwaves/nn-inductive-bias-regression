2fcb480c9f882f88f39030a566cadae4b0ee1ab3
Andrei <inwaves@live.com>
2022-05-30 12:01:42 +0100

    Scriptzzz

--- scripts/no_ablation/parabola/parabola-5k.sh
+++ scripts/no_ablation/parabola/parabola-5k.sh
@@ -4,7 +4,7 @@ num_iter=1
 
 for ((i=1;i<=num_iter;i++))
 do
-  python3 1d_regression.py --tag=parabola-es-5000 --early_stopping=True --num_epochs=100000 --lr_schedule=plateau --dataset=parabola --generalisation_task=baseline --model_type=ASIShallowRelu --hidden_units=5000 --learning_rate=0.0005 --adjust_data_linearly=True
+  python3 1d_regression.py --tag=parabola-es-5000 --early_stopping=True --num_epochs=100000 --lr_schedule=plateau --dataset=parabola --generalisation_task=baseline --model_type=ShallowRelu --hidden_units=5000 --learning_rate=0.0005 --adjust_data_linearly=True
 done
 
 end=$(date +%s)
--- scripts/no_ablation/square/square_var_500.sh
+++ scripts/no_ablation/square/square_var_500.sh
@@ -4,7 +4,7 @@ num_iter=1
 
 for ((i=1;i<=num_iter;i++))
 do
-  python3 1d_regression.py --tag=sq500 --dataset=square --generalisation_task=baseline --model_type=ASIShallowRelu --hidden_units=500 --learning_rate=0.001 --adjust_data_linearly=True --early_stopping=True --num_epochs=100000
+  python3 1d_regression.py --tag=sq500 --dataset=square --generalisation_task=baseline --model_type=ShallowRelu --hidden_units=500 --learning_rate=0.001 --adjust_data_linearly=True --early_stopping=True --num_epochs=100000
 done
 
 end=$(date +%s)
