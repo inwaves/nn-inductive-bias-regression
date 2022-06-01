2542ac6316d6f32cbc8a3cc7257615745dd04dd0
Andrei <inwaves@live.com>
2022-06-01 19:04:48 +0100

    MLP for extrapolation

--- scripts/extrapolation/model_ablation/mlp-150.sh
+++ scripts/extrapolation/model_ablation/mlp-150.sh
@@ -4,7 +4,7 @@ num_iter=1
 
 for ((i=1;i<=num_iter;i++))
 do
-  python3 1d_regression.py --tag=mlp-"$1"-150 --early_stopping=True --lr_schedule=plateau --dataset="$1" --generalisation_task=interpolation --model_type=MLP --hidden_units=150 --learning_rate=0.0005 --adjust_data_linearly=True
+  python3 1d_regression.py --tag=mlp-"$1"-150 --early_stopping=True --lr_schedule=plateau --dataset="$1" --generalisation_task=extrapolation --model_type=MLP --hidden_units=150 --learning_rate=0.0005 --adjust_data_linearly=True
 done
 
 end=$(date +%s)
