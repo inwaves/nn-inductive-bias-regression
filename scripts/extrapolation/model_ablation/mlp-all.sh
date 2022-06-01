ac5fd6f302ed5ad467d2997117f5aa125ea74cc1
Andrei <inwaves@live.com>
2022-06-01 11:04:16 +0100

    MLP for interpolation & extrapolation

---
+++ scripts/extrapolation/model_ablation/mlp-all.sh
@@ -0,0 +1,12 @@
+filenames="mlp-10.sh mlp-100.sh mlp-150.sh mlp-500.sh"
+
+for val in $filenames;
+do
+    for dataset in $2;
+    do
+      for ((i=0; i<$1; i++));
+      do
+      sbatch ./scripts/extrapolation/model_ablation/mlp-sub-script.sh $val $dataset;
+      done
+    done
+done
