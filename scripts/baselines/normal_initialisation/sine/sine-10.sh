94fe3c9a8c1ffa56f2b3dffc1c67adbe3d84590a
Andrei <inwaves@live.com>
2022-06-01 13:33:20 +0100

    normal script

--- scripts/baselines/normal_initialisation/sine/sine-10.sh
+++ scripts/baselines/normal_initialisation/sine/sine-10.sh
@@ -4,7 +4,7 @@ num_iter=1
 
 for ((i=1;i<=num_iter;i++))
 do
-  python3 1d_regression.py --tag=sq-normal-10 --dataset=sine  --generalisation_task=baseline --model_type=ASIShallowRelu --hidden_units=10 --learning_rate=0.1 --adjust_data_linearly=True --early_stopping=True --num_epochs=100000
+  python3 1d_regression.py --tag=sin-normal-10 --dataset=sine --init=normal  --generalisation_task=baseline --model_type=ASIShallowRelu --hidden_units=10 --learning_rate=0.1 --adjust_data_linearly=True --early_stopping=True --num_epochs=100000
 done
 
 end=$(date +%s)
--- scripts/baselines/normal_initialisation/sine/sine-5k.sh
+++ scripts/baselines/normal_initialisation/sine/sine-5k.sh
@@ -4,7 +4,7 @@ num_iter=1
 
 for ((i=1;i<=num_iter;i++))
 do
-  python3 1d_regression.py --tag=sq-normal-5000 --dataset=sine  --generalisation_task=baseline --model_type=ASIShallowRelu --hidden_units=5000 --learning_rate=0.0005 --adjust_data_linearly=True --early_stopping=True --num_epochs=100000
+  python3 1d_regression.py --tag=sin-normal-5000 --dataset=sine --init=normal  --generalisation_task=baseline --model_type=ASIShallowRelu --hidden_units=5000 --learning_rate=0.0005 --adjust_data_linearly=True --early_stopping=True --num_epochs=100000
 done
 
 end=$(date +%s)
