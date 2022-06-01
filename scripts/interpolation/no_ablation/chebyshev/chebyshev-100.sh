b4ca4b1e946a7bf7e73fe7cad1b432205edbf7a4
Andrei <inwaves@live.com>
2022-06-01 12:43:19 +0100

    Fix model for ASI

--- scripts/interpolation/no_ablation/chebyshev/chebyshev-100.sh
+++ scripts/interpolation/no_ablation/chebyshev/chebyshev-100.sh
@@ -4,7 +4,7 @@ num_iter=1
 
 for ((i=1;i<=num_iter;i++))
 do
-  python3 1d_regression.py --tag=cheby-es-100 --early_stopping=True --num_datapoints=11 --num_epochs=100000 --lr_schedule=plateau --dataset=chebyshev_polynomial --generalisation_task=interpolation --model_type=ShallowRelu --hidden_units=100 --learning_rate=0.01 --adjust_data_linearly=True
+  python3 1d_regression.py --tag=cheby-es-100 --early_stopping=True --num_datapoints=11 --num_epochs=100000 --lr_schedule=plateau --dataset=chebyshev_polynomial --generalisation_task=interpolation --model_type=ASIShallowRelu --hidden_units=100 --learning_rate=0.01 --adjust_data_linearly=True
 done
 
 end=$(date +%s)
--- scripts/interpolation/no_ablation/linear/linear-500.sh
+++ scripts/interpolation/no_ablation/linear/linear-500.sh
@@ -4,7 +4,7 @@ num_iter=1
 
 for ((i=1;i<=num_iter;i++))
 do
-  python3 1d_regression.py --tag=lin-unadjusted-500 --early_stopping=True --num_epochs=100000 --lr_schedule=plateau --dataset=linear --generalisation_task=interpolation --model_type=ShallowRelu --hidden_units=500 --learning_rate=0.005 --adjust_data_linearly=False
+  python3 1d_regression.py --tag=lin-unadjusted-500 --early_stopping=True --num_epochs=100000 --lr_schedule=plateau --dataset=linear --generalisation_task=interpolation --model_type=ASIShallowRelu --hidden_units=500 --learning_rate=0.005 --adjust_data_linearly=False
 done
 
 end=$(date +%s)
